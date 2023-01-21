cc7d92640c732b8c8151336d0f5ce862b49c60c2
Valentin447 <valentin@myttk.ru>
2023-01-21 21:52:52 +0700

    Fix3

--- HomeWork/HW_1/task1.java
+++ HomeWork/HW_1/task1.java
@@ -1,9 +1,9 @@
-package HomeWork;
 // Вычислить сумму чисел от 1 до n, вычислить n! произведение чисел от 1 до n
+
+package HomeWork.HW_1;
 
 public class task1 {
   public static void main(String[] args) {
-    
+
   }
-  
 }
